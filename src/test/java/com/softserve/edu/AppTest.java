526d48736f3f728508af09020317e365ab0dbcc8
bebebe <hahaha123@gmail.com>
2019-04-09 10:57:46 +0300

    Update Tests

--- src/test/java/com/softserve/edu/AppTest.java
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -9,7 +9,7 @@ public class AppTest {
 	public void testApp1() {
 		Calc calc = new Calc();
 		double expected = 5;
-		double actual = calc.add(2, 2);
+		double actual = calc.add(2, 3);
 		Assert.assertEquals(expected, actual, 0.001);
 	}
 
@@ -17,13 +17,14 @@ public class AppTest {
 	public void testApp2() {
 		Calc calc = new Calc();
 		double expected = 8;
-		double actual = calc.add(2, 5);
+		double actual = calc.add(3, 5);
 		Assert.assertEquals(expected, actual, 0.001);
 	}
 
 	@Test
 	public void testApp3() {
-		System.out.println("getProperty IS_JENKINS = " + System.getProperty("IS_JENKINS"));
+		System.out.println("getProperty my.variable = " + System.getProperty("my.variable"));
+		System.out.println("getenv GIT_EDITOR = " + System.getenv("GIT_EDITOR"));
 		System.out.println("getenv IS_JENKINS = " + System.getenv("IS_JENKINS"));
 	}
 
